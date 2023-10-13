e3f95a091076a1b1f21fbcd5d4362635675f682b
Robin <robinkelsick@gmail.com>
2023-10-13 11:02:25 -0400

    added styling changes to nav

--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -3,17 +3,29 @@ import { HiMenuAlt1 } from 'react-icons/hi';
 const NavMenu = () => {
   return (
     <div className='navbar bg-neutral'>
-      <div className='navbar-start'>
-        <div className='dropdown'>
-          <label tabIndex={0} className='btn btn-ghost lg:hidden'>
+      <div className='min-w-full min-h-full lg:justify-end navbar-start'>
+        <div className='dropdown lg:hidden'>
+          <label tabIndex={0} className='btn btn-ghost'>
             <HiMenuAlt1 className='text-base-100' />
           </label>
-          <ul className='flex flex-row justify-items-end text-base-100'>
-            <li>Home</li>
-            <li>New Quiz</li>
-            <li>High Scores</li>
-            <li>Discord Bots</li>
-            <li>Contact the Dev</li>
+        </div>
+        <div className='hidden lg:flex lg:flex-col text-base-100 '>
+          <ul>
+            <li>
+              <a>Home</a>
+            </li>
+            <li>
+              <a>Categories</a>
+            </li>
+            <li>
+              <a>Home</a>
+            </li>
+            <li>
+              <a>Home</a>
+            </li>
+            <li>
+              <a>Home</a>
+            </li>
           </ul>
         </div>
       </div>
